7342724f9cd89c049c71cde4196193cf40a007f1
ZAITERNouara <zaiternouara5@gmail.com>
2021-04-05 18:26:09 +0100

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -9,23 +9,24 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
+
 public class Main extends Application{
-	
+
 	public void start(Stage primaryStage) {
 		try {
-			
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MainController.fxml"));	
+
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MainController.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 	        primaryStage.setTitle("Projet entropot");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
+
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
